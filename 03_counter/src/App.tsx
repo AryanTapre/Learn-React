7a7c9dc1a77b421759a254235845b0926455ec33
AryanTapre <taprearyan7@gmail.com>
2024-06-14 21:22:06 +0530

    useState

---
+++ 03_counter/src/App.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable prefer-const */
+
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let [count, setCount] = useState(() => {
+    return 0;
+  })
+
+  function handleClick() {
+    setCount(count += 1);
+  }
+  function resetCounter() {
+    setCount(0);
+  }
+
+  return (
+    <>
+      <h3>Current counter value : {count}</h3>
+      <button id='button' onClick={handleClick}> add value</button> &nbsp;
+      <button id='reset-button' onClick={resetCounter}> Reset value</button>
+    </>
+  )
+}
+
+export default App
