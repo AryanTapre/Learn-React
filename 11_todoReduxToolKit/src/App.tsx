e57b627c3ee33cc80dc893532d372aa74acd52ad
AryanTapre <taprearyan7@gmail.com>
2024-06-20 22:57:40 +0530

    RTK initialzed for 11_todo

---
+++ 11_todoReduxToolKit/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='h-screen w-screen bg-gray-800 text-white flex justify-center items-center text-2xl '>
+            <h1>Learning about Redux-Toolkit & React-Redux</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
